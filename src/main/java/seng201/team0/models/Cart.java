7815462efa8e0b015c5d6a37733bc1ece1447a13
ale155 <ale155@uclive.ac.nz>
2024-04-28 17:18:53 +1200

    Method to increment the amount resource into the size

--- src/main/java/seng201/team0/models/Cart.java
+++ src/main/java/seng201/team0/models/Cart.java
@@ -4,11 +4,19 @@ public class Cart {
     private String typeResource;
     private float speed;
     private int size;
-
+    private int currentAmount = 0;
+    private int amount;
     public Cart(String inputResoure, float inputSpeed, int inputSize ) {
         this.typeResource = inputResoure;
         this.speed = inputSpeed;
         this.size = inputSize;
     }
-
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+    public int getAmount(){return amount;}
+    public void incrementResource() {
+        currentAmount += getAmount();
+    }
+    public int getCurrentSize(){return currentAmount;}
 }
