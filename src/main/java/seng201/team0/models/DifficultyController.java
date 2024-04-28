6de43ffb6bec2faa4e6bb89a1ba918e38e98c53f
Ngoc Le <nle65@uclive.ac.nz>
2024-04-28 13:59:54 +1200

    edits to RoundManager and DifficultyController

--- src/main/java/seng201/team0/models/DifficultyController.java
+++ src/main/java/seng201/team0/models/DifficultyController.java
@@ -1,41 +1,52 @@
 package seng201.team0.models;
-import seng201.team0.models.RoundManager;
 
 public class DifficultyController {
-    private int Easy = 1;
-    private int Moderate = 2;
-    private int Challenging = 3;
-    private int CurrentDifficulty = Easy;
-    private int LevelCounter = 1;
+    private int easy = 1;
+    private int moderate = 2;
+    private int challenging = 3;
+    private int currentDifficulty = easy;
+    private int levelCounter = 1;
 
     public void onChooseDifficultyEasy(){
-        CurrentDifficulty = Easy;
+        currentDifficulty = easy;
     }
 
     public void onChooseDifficultyModerate(){
-        CurrentDifficulty = Moderate;
+        currentDifficulty = moderate;
     }
 
     public void onChooseDifficultyChallenging(){
-        CurrentDifficulty = Challenging;
+        currentDifficulty = challenging;
     }
 
     public void IncrementLevel() {
-        if (CurrentDifficulty == Easy) {
-            LevelCounter += 1;
-        } else if (CurrentDifficulty == Moderate) {
-            LevelCounter += 2;
+        if (currentDifficulty == easy) {
+            levelCounter += 1;
+        } else if (currentDifficulty == moderate) {
+            levelCounter += 2;
         } else {
-            LevelCounter += 3;
+            levelCounter += 3;
         }
     }
     public int getLevelCounter() {
-        return LevelCounter;
+        return levelCounter;
     }
 
-    // Sets up various game variables according to the current round counter and difficulty
-    public void roundDifficultySet() {
-        setTrackDistance();
+    /* Sets up various game variables according to the current round counter and difficulty
+       on a given RoundManager object
+    * */
+    public void roundDifficultySet(RoundManager rm) {
+
+        if (currentDifficulty == easy) {
+            // Set variables for easy mode
+            rm.setTrackDistance(10);
+        } else if (currentDifficulty == moderate) {
+            // Set variables for moderate mode
+            rm.setTrackDistance(8);
+        } else if (currentDifficulty == challenging) {
+            // Set variables for challenging mode
+            rm.setTrackDistance(5);
+        }
     }
 
 }
--- src/main/java/seng201/team0/models/RoundManager.java
+++ src/main/java/seng201/team0/models/RoundManager.java
@@ -1,9 +1,15 @@
 package seng201.team0.models;
 
+import java.util.ArrayList;
+
 public class RoundManager {
     private float trackDistance;
-    //private ArrayList<Cart> carts;        // Yet to implement the cart class
+    private ArrayList<Cart> carts;
 
+    public RoundManager(float inputDistance, ArrayList<Cart> inputCarts) {
+        this.trackDistance = inputDistance;
+        this.carts = inputCarts;
+    }
 
     public void setTrackDistance(float distance) {
         trackDistance = distance;
