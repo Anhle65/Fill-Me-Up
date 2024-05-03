2e3215f81ff19f30cd0e6758a0bad01b7aab1759
ale155 <ale155@uclive.ac.nz>
2024-05-03 14:29:04 +1200

    Create a setup page to start the game

--- src/main/java/seng201/team0/gui/SetupScreenController.java
+++ src/main/java/seng201/team0/gui/SetupScreenController.java
@@ -1,18 +1,14 @@
 package seng201.team0.gui;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.control.*;
 import seng201.team0.models.Tower;
-import seng201.team0.services.TowerManager;
+import seng201.team0.services.EnvironmentManager;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-public class SetupScreenController {
+public class SetupScreenController{
     @FXML
     private Label towerType;
     @FXML
@@ -45,25 +41,42 @@ public class SetupScreenController {
     private Button towerStat5;
     @FXML
     private Button towerStat6;
-    private TowerManager towerManager;
+    @FXML
+    private ChoiceBox<String> difficultyOptions = new ChoiceBox<>();
+    private EnvironmentManager environmentManager;
     private int selectedTowerIndex = -1;
     private final Tower[] selectedTowers = new Tower[3];
-    public SetupScreenController(TowerManager tower){
-        this.towerManager = tower;
+    public SetupScreenController(EnvironmentManager environmentManager){
+        this.environmentManager = environmentManager;
     }
     @FXML
     private void onAcceptClicked() {
-        towerManager.setName(nameTextField.getText());
-        towerManager.setTowerList(Arrays.stream(selectedTowers).filter((Objects::nonNull)).toList());
-        towerManager.closeSetupScreen();
+        environmentManager.setName(nameTextField.getText());
+        environmentManager.setTowerList(Arrays.stream(selectedTowers).filter((Objects::nonNull)).toList());
+        environmentManager.closeSetupScreen();
     }
-    public void initialize(Stage stage){
+
+
+
+    /**
+     * Initialize the setup page
+     */
+
+    public void initialize(){
+//        this.difficultyOptions = new ChoiceBox();
+        difficultyOptions.getItems().add("Easy");
+        difficultyOptions.getItems().add("Moderate");
+        difficultyOptions.getItems().add("Challenge");
+//        difficultyOptions.setValue("Choose the difficulty");
+//        difficultyOptions.getSelectionModel();
+//        System.out.println(environmentManager.getDifficulty());
+
         List<Button> selectedTowerButtons = List.of(selectedTower1, selectedTower2, selectedTower3);
         List<Button> towerButtons = List.of(towerStat1, towerStat2, towerStat3, towerStat4, towerStat5, towerStat6);
         for (int i = 0; i < towerButtons.size(); i++) {
             int finalI = i; // variables used within lambdas must be final
             towerButtons.get(i).setOnAction(event -> {
-                updateStats(towerManager.getDefaultTowers().get(finalI));
+                updateStats(environmentManager.getDefaultTowers().get(finalI));
                 selectedTowerIndex = finalI;
                 towerButtons.forEach(button -> {
                     if (button == towerButtons.get(finalI)) {
@@ -78,22 +91,24 @@ public class SetupScreenController {
             int finalI = i; // variables used within lambdas must be final
             selectedTowerButtons.get(i).setOnAction(event -> {
                 if (selectedTowerIndex != -1) {
-                    selectedTowerButtons.get(finalI).setText(towerManager.getDefaultTowers().get(selectedTowerIndex).getType());
-                    selectedTowers[finalI] = towerManager.getDefaultTowers().get(selectedTowerIndex);
+                    selectedTowerButtons.get(finalI).setText(environmentManager.getDefaultTowers().get(selectedTowerIndex).getType());
+                    selectedTowers[finalI] = environmentManager.getDefaultTowers().get(selectedTowerIndex);
                 }
             });
         }
-//        roundSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-//            for (int i = 0; i < selectedRocketButtons.size(); i++) {
-//                selectedRocketButtons.get(i).setDisable(i >= newValue.intValue());
-//            }
-//        });
+        roundSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            for (int i = 0; i < selectedTowerButtons.size(); i++) {
+                selectedTowerButtons.get(i).setDisable(i >= newValue.intValue());
+            }
+        });
     }
     private void updateStats(Tower tower) {
         towerType.setText(tower.getType());
         towerCost.setText(String.valueOf(tower.getCost()));
         levelTower.setText(String.valueOf(tower.getLevel()));
         recoveryTime.setText(String.valueOf(tower.getRecoveryTime()));
-        resourceAmount.setText(String.valueOf(tower.getCapacity()));
+        resourceAmount.setText(String.valueOf(tower.getResourceAmount()));
     }
+
+    private void setDifficulty(){difficultyOptions.getValue();}
 }
