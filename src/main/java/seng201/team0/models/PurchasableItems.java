83c8aeec84ae8505fdb712681b3d0c0cbdce03d0
Anhle65 <levananh235813@gmail.com>
2024-05-08 23:53:21 +1200

    Add more Javadoc in the PurchasableItems class

--- src/main/java/seng201/team0/models/PurchasableItems.java
+++ src/main/java/seng201/team0/models/PurchasableItems.java
@@ -3,6 +3,9 @@ package seng201.team0.models;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Purchasable class to keep track all the items can be bought from shop
+ */
 public class PurchasableItems {
     private String nameItem;
     private float improvedTime;
@@ -10,18 +13,39 @@ public class PurchasableItems {
     private int cost;
     private String newType;
     private List<PurchasableItems> items = new ArrayList<>();
+
+    /**
+     * This constructor to initialize tower as an item in shop
+     * @param name
+     * @param tower
+     */
     public PurchasableItems(String name, Tower tower){
         this.nameItem = name;
         this.improvedAmountResource = 0;
         this.improvedTime = 0;
         this.cost = tower.getCost();
     }
+
+    /**
+     * This constructor to initialize an upgrade card which can be used for tower
+     * @param name
+     * @param improvedAmountResource
+     * @param improvedTime
+     * @param cost
+     */
     public PurchasableItems(String name, int improvedAmountResource, float improvedTime, int cost) {
         this.nameItem = name;
         this.improvedAmountResource = improvedAmountResource;
         this.improvedTime = improvedTime;
         this.cost = cost;
     }
+
+    /**
+     * This constructor to initialize the changing card which can be used to change the type of tower
+     * @param name
+     * @param newType
+     * @param cost
+     */
     public PurchasableItems(String name, String newType, int cost){
         this.nameItem = name;
         this.improvedAmountResource = 0;
