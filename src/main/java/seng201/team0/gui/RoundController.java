dae5e112976aa260f3d2a328c3fcbc6dcbfffd0c
Ngoc Le <nle65@uclive.ac.nz>
2024-05-09 14:12:23 +1200

    changed round game screen fxml

--- src/main/java/seng201/team0/gui/RoundController.java
+++ src/main/java/seng201/team0/gui/RoundController.java
@@ -1,33 +1,27 @@
 package seng201.team0.gui;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import seng201.team0.models.Cart;
-import seng201.team0.models.RoundManager;
-import seng201.team0.models.Tower;
+
+
+import javax.swing.text.html.ImageView;
+import java.awt.event.ActionEvent;
 
 public class RoundController {
-    @FXML
-    private TextField cartInfor;
 
     @FXML
-    private Button towerButton;
-    private Tower tower;
-    private Cart cart;
-    // Initialize the game
-    public void init(Stage stage){
-        tower = new Tower("water",40,20,3);
-        cart = new Cart("water", 5f,100);
-        cart.setAmount(tower.getResourceAmount());
-    }
-    public void onClickedTower() {
-        System.out.println("Button has been clicked");
-        cart.incrementResource();
+    private ImageView cartImageView;
 
-        int count = cart.getCurrentSize();
-        cartInfor.setText(Integer.toString(count));
+
+
+    public void onPlayButtonClicked() {
+
     }
+
+    public void initialize() {
+
+    }
+
+
+
 }
