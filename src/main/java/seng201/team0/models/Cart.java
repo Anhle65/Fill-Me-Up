25f5ccfbf37c98ebd307eecd1137262bdd1d4c11
Anhle65 <levananh235813@gmail.com>
2024-05-12 21:08:32 +1200

    Add get methods, increment method in class Cart and write unittest for Cart

--- src/main/java/seng201/team0/models/Cart.java
+++ src/main/java/seng201/team0/models/Cart.java
@@ -1,22 +1,54 @@
 package seng201.team0.models;
 
 public class Cart {
-    private String typeResource;
-    private float speed;
-    private int size;
+    private final String typeResourceCart;
+    private final float speed;
+    private final int sizeOfCart;
     private int currentAmount = 0;
-    private int amount;
     public Cart(String inputResoure, float inputSpeed, int inputSize ) {
-        this.typeResource = inputResoure;
+        this.typeResourceCart = inputResoure;
         this.speed = inputSpeed;
-        this.size = inputSize;
+        this.sizeOfCart = inputSize;
     }
-    public void setAmount(int amount){
-        this.amount = amount;
+
+    /**
+     * Get the type of cart
+     * @return typeResourceCart
+     */
+    public String getTypeResourceCart(){return this.typeResourceCart;}
+
+    /**
+     * Get the size of cart
+     * @return sizeOfCart
+     */
+    public int getSizeOfCart(){return this.sizeOfCart;}
+
+    /**
+     * Get the speed of cart
+     * @return speed
+     */
+    public float getSpeed(){return this.speed;}
+
+    /**
+     * Increment the amount resource into cart if the input is the same type with cart's type
+     */
+    public void incrementAmountResourceIntoCart(Tower tower) {
+        if(tower.getType().equals(this.typeResourceCart)){
+            currentAmount += tower.getResourceAmount();
+            if(currentAmount > sizeOfCart)
+                currentAmount = sizeOfCart;
+        }
     }
-    public int getAmount(){return amount;}
-    public void incrementResource() {
-        currentAmount += getAmount();
-    }
-    public int getCurrentSize(){return currentAmount;}
+
+    /**
+     * Get the current amount that cart has been filled
+     * @return currentAmount
+     */
+    public int getCurrentAmountOfCart(){return currentAmount;}
+
+    /**
+     * Check if the cart has been fulled or not
+     * @return true if the cart has been filled up, false otherwise
+     */
+    public boolean isCartFilledUp(){return currentAmount == sizeOfCart;}
 }
