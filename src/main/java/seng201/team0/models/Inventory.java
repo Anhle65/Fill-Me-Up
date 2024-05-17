c54c1a696ad49126ab66a371b483d00af199fa08
Anhle65 <levananh235813@gmail.com>
2024-05-18 00:53:24 +1200

    Create the Inventory class

--- src/main/java/seng201/team0/models/Inventory.java
+++ src/main/java/seng201/team0/models/Inventory.java
@@ -1,4 +1,12 @@
 package seng201.team0.models;
 
+import seng201.team0.services.EnvironmentManager;
+
+import java.util.List;
+
 public class Inventory {
+    private int playerCoins;
+    private List<Tower> reservedTowerList;
+    private EnvironmentManager environmentManager;
+
 }
