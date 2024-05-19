9dec424ab8a2802dae6772b3992e667809d1c5b6
Anhle65 <levananh235813@gmail.com>
2024-05-19 20:17:06 +1200

    Add shop class and the initialization of ShopController

--- src/main/java/seng201/team0/gui/ShopController.java
+++ src/main/java/seng201/team0/gui/ShopController.java
@@ -3,15 +3,13 @@ package seng201.team0.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import seng201.team0.models.PurchasableItems;
+import seng201.team0.models.PurchasableItem;
 import seng201.team0.models.Shop;
+import seng201.team0.models.UpgradeItems;
 import seng201.team0.models.Tower;
 import seng201.team0.services.EnvironmentManager;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 
 public class ShopController {
@@ -30,21 +28,25 @@ public class ShopController {
     @FXML
     private Button tower3;
     private EnvironmentManager environmentManager;
-    private int selectedTowerIndex = -1;
-    private int selectedCardIndex = -1;
-    private List<PurchasableItems> listTowersCanBuy = new ArrayList<>();
-    private List<PurchasableItems> listUpgradeCards = new ArrayList<>();
+    private Shop shop;
+    private int selectedItemIndex = -1;
+    private List<Tower> listTowersInShop = new ArrayList<>();
+    private List<UpgradeItems> listUpgradeCardsInShop = new ArrayList<>();
     private final String[] TYPE_RESOURCES = {"Water", "Fire", "Gold", "Coal", "Diamond"};
     private final float[] TIME_ENHANCEMENT = {1, 1.5f, 2, 2.5f, 3};
     private final Integer[] COST = {10, 15, 25, 40, 50};
     private final Integer[] RESOURCE_ENHANCEMENT = {5, 7, 10, 12, 15};
-    public ShopController(EnvironmentManager environmentManager) {this.environmentManager = environmentManager;}
+    public ShopController(EnvironmentManager environmentManager, Shop shop) {
+        this.environmentManager = environmentManager;
+        this.shop = shop;
+    }
 
     /**
      * Initialize 3 random upgrade cards and 3 random towers which player can buy from shop to upgrade tower's statistics
      * or have new tower. The price is different depending on how good the upgrade card can do to the tower.
      */
     public void initialize(){
+//        playerCoins.setText(String.valueOf(environmentManager.getPlayerCoins()));
         List<Button> listUpgradeCardButtons = List.of(upgradeCard1, upgradeCard2, upgradeCard3);
         List<Button> listTowersInShopButtons = List.of(tower1, tower2, tower3);
         List<Button> listItemsButton = List.of(upgradeCard1, upgradeCard2, upgradeCard3, tower1, tower2, tower3);
@@ -57,39 +59,52 @@ public class ShopController {
                 randomTypeResource.nextInt(TYPE_RESOURCES.length), randomTypeResource.nextInt(TYPE_RESOURCES.length));
 
         // Create 3 random tower
-        listTowersCanBuy.addAll(List.of(
-                new PurchasableItems(new Tower(TYPE_RESOURCES[towerRandomTypeIndexes.get(0)], 40, 20, 6)),
-                new PurchasableItems(new Tower(TYPE_RESOURCES[towerRandomTypeIndexes.get(1)], 40, 20, 6)),
-                new PurchasableItems(new Tower(TYPE_RESOURCES[towerRandomTypeIndexes.get(2)], 40, 20, 6))
+        listTowersInShop.addAll(List.of(
+                new Tower(TYPE_RESOURCES[towerRandomTypeIndexes.get(0)], 40, 20, 6),
+                new Tower(TYPE_RESOURCES[towerRandomTypeIndexes.get(1)], 40, 20, 6),
+                new Tower(TYPE_RESOURCES[towerRandomTypeIndexes.get(2)], 40, 20, 6)
         ));
+        shop.setListTowersInShop(listTowersInShop);
 
         // Create 3 random upgrade card
-        listUpgradeCards.addAll(List.of(
-                new PurchasableItems("Upgrade Time", 0, TIME_ENHANCEMENT[timeIndex], COST[timeIndex]),
-                new PurchasableItems("Upgrade\nAmount", RESOURCE_ENHANCEMENT[enhancedResourceIndex], 0, COST[enhancedResourceIndex]),
-                new PurchasableItems("Changing Type\nTower to " + TYPE_RESOURCES[randomResource.nextInt(TYPE_RESOURCES.length)], TYPE_RESOURCES[randomResource.nextInt(TYPE_RESOURCES.length)] , 60)
+        listUpgradeCardsInShop.addAll(List.of(
+                new UpgradeItems("Upgrade Time", 0, TIME_ENHANCEMENT[timeIndex], COST[timeIndex]),
+                new UpgradeItems("Upgrade\nAmount", RESOURCE_ENHANCEMENT[enhancedResourceIndex], 0, COST[enhancedResourceIndex]),
+                new UpgradeItems("Changing Type\nTower to " + TYPE_RESOURCES[randomResource.nextInt(TYPE_RESOURCES.length)], TYPE_RESOURCES[randomResource.nextInt(TYPE_RESOURCES.length)] , 60)
         ));
+        shop.setListUpgradeCardsInShop(listUpgradeCardsInShop);
+        System.out.println("This is the first upgrade item in Shop: " + shop.getListUpgradeCardsInShop().get(0).getName());
 
+        ArrayList<PurchasableItem> allItemsInShop = new ArrayList<>();
+        for(UpgradeItems card : listUpgradeCardsInShop){
+            allItemsInShop.add(card);
+        }
+        for(Tower tower : listTowersInShop){
+            allItemsInShop.add(tower);
+        }
+
+        System.out.println("Length of items in Shop: " + allItemsInShop.size());
         // Set the upgrade card in shop
-        for (int i = 0; i < listUpgradeCardButtons.size(); i++) {
-            int finalI = i; // variables used within lambdas must be final
-            PurchasableItems item = listUpgradeCards.get(finalI);
-            listUpgradeCardButtons.get(finalI).setText(item.getNameItem());
-        }
-
-        // Set the tower in shop
-        for (int i = 0; i < listTowersInShopButtons.size(); i++) {
-            int finalI = i; // variables used within lambdas must be final
-            PurchasableItems item = listTowersCanBuy.get(finalI);
-            listTowersInShopButtons.get(finalI).setText("Tower: " + item.getNameItem());
-        }
+//        for (int i = 0; i < listUpgradeCardButtons.size(); i++) {
+//            int finalI = i; // variables used within lambdas must be final
+//            UpgradeItems item = listUpgradeCardsInShop.get(finalI);
+//            listUpgradeCardButtons.get(finalI).setText(item.getNameItem());
+//        }
+//
+//        // Set the tower in shop
+//        for (int i = 0; i < listTowersInShopButtons.size(); i++) {
+//            int finalI = i; // variables used within lambdas must be final
+//            Tower item = listTowersInShop.get(finalI);
+//            listTowersInShopButtons.get(finalI).setText("Tower:\n" + item.getType());
+//        }
 
         // Change color of the button which is chosen by player
         for (int i = 0; i < listItemsButton.size(); i++) {
             int finalI = i; // variables used within lambdas must be final
+            PurchasableItem item = allItemsInShop.get(finalI);
+            listItemsButton.get(finalI).setText(item.getName());
             listItemsButton.get(i).setOnAction(event -> {
-//                updateStats(environmentManager.getDefaultTowers().get(finalI));
-                selectedTowerIndex = finalI;
+                selectedItemIndex = finalI;
                 listItemsButton.forEach(button -> {
                     if (button == listItemsButton.get(finalI)) {
                         button.setStyle("-fx-background-color: pink; -fx-text-fill: black; -fx-font-size: 10px; -fx-font-family: Verdana; -fx-font-weight: bold; -fx-background-radius: 5;");
@@ -102,6 +117,7 @@ public class ShopController {
     }
     public void onBuyClicked(){
         System.out.println("Clicked on Buy button");
+
     }
 
     public void onNextClicked(){
--- src/main/java/seng201/team0/models/Shop.java
+++ src/main/java/seng201/team0/models/Shop.java
@@ -1,20 +1,91 @@
 package seng201.team0.models;
 
+import seng201.team0.services.EnvironmentManager;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Shop {
-    private int playerCoins;
-    private List<PurchasableItems> listTowersInShop = new ArrayList<>();
-    private List<PurchasableItems> listUpgradeCards = new ArrayList<>();
-    public int getPlayerCoins(){
-        return this.playerCoins;
+//    private final Consumer<Shop> shopScreenLauncher;
+//    private final Runnable clearScreen;
+    private EnvironmentManager environmentManager;
+    private List<Tower> listTowersInShop;
+    private List<UpgradeItems> listUpgradeCardsInShop = new ArrayList<>();
+    private List<String> towerType = List.of("Water", "Fire", "Gold", "Coal", "Diamond");
+    private int currentCoin;
+    public Shop(){
+//        this.currentCoin = environmentManager.getPlayerCoins();
     }
-    public void buy(){
+//    private final String[] TYPE_RESOURCES = {"Water", "Fire", "Gold", "Food", "Coal", "Diamond"};
+//    private final float[] TIME_ENHANCEMENT = {1, 1.5f, 2, 2.5f, 3};
+//    private final Integer[] COST = {10, 15, 25, 40, 50};
+//    private final Integer[] RESOURCE_ENHANCEMENT = {5, 7, 10, 12, 15};
+//    public Shop(){
+//        Random randomResource = new Random();
+//        Random randomTimeUpgrade = new Random();
+//        Random randomTypeResource = new Random();
+//        int enhancedResourceIndex=  randomResource.nextInt(RESOURCE_ENHANCEMENT.length);
+//        int timeIndex = randomTimeUpgrade.nextInt(TIME_ENHANCEMENT.length);
+//        List<Integer> towerRandomTypeIndexes = List.of(randomTypeResource.nextInt(TYPE_RESOURCES.length),
+//                randomTypeResource.nextInt(TYPE_RESOURCES.length), randomTypeResource.nextInt(TYPE_RESOURCES.length));
+//        listTowersInShop.addAll(List.of(
+//                new PurchasableItems(new Tower(TYPE_RESOURCES[towerRandomTypeIndexes.get(0)], 40, 20, 6)),
+//                new PurchasableItems(new Tower(TYPE_RESOURCES[towerRandomTypeIndexes.get(1)], 40, 20, 6)),
+//        ));
+//        listUpgradeCards.addAll(List.of(
+//                new PurchasableItems("Upgrade time", 0, TIME_ENHANCEMENT[timeIndex], COST[timeIndex]),
+//                new PurchasableItems("Upgrade amount resource", RESOURCE_ENHANCEMENT[enhancedResourceIndex], 0, COST[enhancedResourceIndex]),
+//                new PurchasableItems("Change the type of tower" + TYPE_RESOURCES[randomResource.nextInt(TYPE_RESOURCES.length)], TYPE_RESOURCES[randomResource.nextInt(TYPE_RESOURCES.length)] , 60)
+//        ));
+//    }
+//    /**
+//     * Get the list of towers in shop
+//     * @return listTowerInShop
+//     */
+//    public List<PurchasableItems> getListTowersCanBuy(){
+//        return this.listTowersInShop;
+//    }
+//
+//    /**
+//     * Get the list of upgrade items in shop
+//     * @return listUpgradeCards
+//     */
+//    public List<PurchasableItems> getListUpgradeCards(){
+//        return this.listUpgradeCards;
+//    }
+//    /**
+//     * Get the player coins
+//     * @return playerCoins
+//     */
+//    public int getPlayerCoins(){
+//        return this.playerCoins;
+//    }
+//    public Shop(Consumer<Shop>shopScreenLauncher, Runnable clearScreen ){
+//        this.shopScreenLauncher = shopScreenLauncher;
+//        this.clearScreen = clearScreen;
+//    }
+//    public void launchShopScreen() { shopScreenLauncher.accept(this);}
+//    public void setCurrentCoin(){this.currentCoin = environmentManager.getPlayerCoins();}
+    public void buy(UpgradeItems item){
+        if(this.currentCoin >= item.getCost()){
+            this.currentCoin -= item.getCost();
+            environmentManager.getListUpgradeCardsInInventory().add(item);
+        }
+    }
 
+    public void buy(Tower item){
+        if(this.currentCoin >= item.getCost()){
+            this.currentCoin -= item.getCost();
+            environmentManager.getReservedTowerList().add(item);
+        }
     }
-    public void sell(){
+    public void setListTowersInShop(List<Tower> listTowersInShop){
+        this.listTowersInShop = listTowersInShop;
+    }
 
+    public void setListUpgradeCardsInShop(List<UpgradeItems> listUpgradeCardsInShop){
+        this.listUpgradeCardsInShop = listUpgradeCardsInShop;
     }
+
+    public List<UpgradeItems> getListUpgradeCardsInShop(){return this.listUpgradeCardsInShop;}
 }
