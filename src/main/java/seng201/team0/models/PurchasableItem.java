1ba0d2f6996d2ef9bba6abdfc89e2e62970c62ee
ale155 <ale155@uclive.ac.nz>
2024-05-21 14:55:48 +1200

    Remove the redundant

--- src/main/java/seng201/team0/models/PurchasableItem.java
+++ src/main/java/seng201/team0/models/PurchasableItem.java
@@ -2,12 +2,6 @@ package seng201.team0.models;
 
 public class PurchasableItem {
     private String nameItem;
-//    public PurchasableItem(Tower tower){
-//        this.nameItem = tower.getType();
-//    }
-//    public PurchasableItem(String name, int improvedAmountResource, float improvedTime, int cost){
-//        this.nameItem = name;
-//    }
     /**
      * @return name of the Item
      */
