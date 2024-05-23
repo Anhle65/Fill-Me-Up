9748d95d14ad8b82ce13d01c377cb398944c19ed
ale155 <ale155@uclive.ac.nz>
2024-05-23 16:04:31 +1200

    Show cost item on shop screen

--- src/main/java/seng201/team0/models/PurchasableItem.java
+++ src/main/java/seng201/team0/models/PurchasableItem.java
@@ -2,10 +2,17 @@ package seng201.team0.models;
 
 public class PurchasableItem {
     private String nameItem;
+    private int itemCost;
     /**
      * @return name of the Item
      */
     public String getName(){
         return nameItem;
     };
+
+    /**
+     * Get the cost of item
+     * @return itemCost
+     */
+    public int getCost(){return itemCost;}
 }
